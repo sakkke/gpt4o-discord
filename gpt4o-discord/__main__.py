991962c9480efa232d95c1ecb7076d9060c2bf1b
sakkke <84666033+sakkke@users.noreply.github.com>
2024-05-14 08:38:13 +0900

    feat: implement hello world bot

--- gpt4o-discord/__main__.py
+++ gpt4o-discord/__main__.py
@@ -1,1 +1,30 @@
-print("hello world")
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+
+
+token = os.getenv("TOKEN")
+if token is None:
+    raise EnvironmentError("TOKEN is not defined")
+client.run(token)
